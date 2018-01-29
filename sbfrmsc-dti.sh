08f83db7f594bdafaad7a0dd57ed31a7157e15e8
terrabyte911 <davidclarkeace@yahoo.co.uk>
2018-01-29 20:00:59 +0000

    Update sbfrmsc-dti.sh

--- sbfrmsc-dti.sh
+++ sbfrmsc-dti.sh
@@ -271,7 +271,7 @@ getString NO  "OpenVPN port: " OPENVPNPORT1 31195
 #getString NO  "Do you want to have some of your users in a chroot jail? " CHROOTJAIL1 YES
 getString NO  "Install Webmin? " INSTALLWEBMIN1 YES
 getString NO  "Install Fail2ban? " INSTALLFAIL2BAN1 YES
-getString NO  "Install OpenVPN? " INSTALLOPENVPN1 NO
+getString NO  "Install OpenVPN? " INSTALLOPENVPN1 YES
 getString NO  "Install SABnzbd? " INSTALLSABNZBD1 NO
 getString NO  "Install Rapidleech? " INSTALLRAPIDLEECH1 NO
 getString NO  "Install Deluge? " INSTALLDELUGE1 NO
